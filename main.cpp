8c5a7573e823a3c1875bce007f14dfb0f6b0dded
Nico Liberato Candio <nico.candio@gmail.com>
2016-02-15 23:30:43 +0100

    [2048]: Deactivate the QML interface,                 will be designed in a second phase

--- main.cpp
+++ main.cpp
@@ -1,18 +1,13 @@
 #include <QGuiApplication>
-#include <QQmlApplicationEngine>
-#include <QtQml/QQmlEngine>
-#include <QtQuick/QQuickView>
-
-#include <QtDeclarative/QDeclarativeEngine>
-
-
-
+// define for the UX... to be used later
+//#include <QQmlApplicationEngine>
+//#include <QtQml/QQmlEngine>
+//#include <QtQuick/QQuickView>
+//#include <QtDeclarative/QDeclarativeEngine>
 
 
 #include "types.h"
 #include "game.h"
-
-
 
 
 int main(int argc, char *argv[])
@@ -20,6 +15,13 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     //The QML interface is deactivated at moment
+
+    /*
+     *   As discussed, this part will manage
+     *   the QML interface, in second phase of
+     *   the project
+     *
+     *
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
 
@@ -30,7 +32,7 @@ int main(int argc, char *argv[])
     QObject *rect = object->findChild<QObject*>("01");
     if (rect)
         rect->setProperty("color", "red");
-
+    */
     game aGame;
 
     aGame.print_grid(mat,DIM,DIM);
